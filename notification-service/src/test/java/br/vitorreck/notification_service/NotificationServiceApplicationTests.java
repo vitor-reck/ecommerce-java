cb2d147020f6eb633aad4c796ccaf1187d124dd6
vitor-reck <vitor_reck.150@hotmail.com>
2025-04-09 12:00:38 -0300

    Initial commit

---
+++ notification-service/src/test/java/br/vitorreck/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.vitorreck.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
